5695b9a2b894f82dc7a78d3d864b1c9cbf538071
emilyirisdev <93999129+emilyirisdev@users.noreply.github.com>
2022-01-27 14:03:28 -0500

    Create practiceArrays.js

---
+++ practiceArrays.js
@@ -0,0 +1,23 @@
+let grades = [10, 5, 13]
+let firstGrade = grades[0]
+console.log(firstGrade)
+
+let grades = [10, 5, 13]
+let secondGrade = grades[1]
+console.log(secondGrade)
+
+let temperatures = [-5, 12, 15, 18, 13]
+let value = temperatures[2]
+console.log(value)
+
+function getFullName(names) {
+return `${names[0]} ${names[1]}`
+}
+
+function describeShopLocation(coordinates) {
+return `The shop is located at ${coordinates[0]}, ${coordinates[1]}`
+}
+
+function describeShopLocation(shop, coordinates) {
+return `The ${shop} is located at ${coordinates[0]}, ${coordinates[1]}`
+}
